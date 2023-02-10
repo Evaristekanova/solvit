ad5cc07cb5b5c2e9ddfd20ab6533d186ae64e2c6
Evaristekanova <dusingizimanaevariste3@gmail.com>
2023-02-10 09:40:38 +0200

    solvit challenges

--- reverse.js
+++ reverse.js
@@ -1,6 +1,6 @@
-const inputString = process.argv[2];
+const orginalString = process.argv[2];
 try {
-  const reversedString = inputString.split("").reverse().join("");
+  const reversedString = orginalString.split("").reverse().join("");
   console.log(reversedString);
 } catch (error) {
   console.log("Enter some string please");
--- vowelConter.js
+++ vowelConter.js
@@ -1,7 +1,7 @@
-const inputString = process.argv[2];
+const orginalString = process.argv[2];
 try {
   const vowels = ["a", "e", "i", "o", "u"];
-  const vowelCount = inputString
+  const vowelCount = orginalString
     .split("")
     .filter((letter) => vowels.includes(letter)).length;
   console.log(vowelCount);
